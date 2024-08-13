2ea229d5e184239d03bf47a28482e6e3c12e7d07
stolomeo <tolomeosebastian@gmail.com>
2024-08-13 19:05:42 -0400

    deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
   return (
-    <header className="flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
-      <div className="flex flex-wrap items-center justify-between gap-5 w-full">
+    <header className="flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[80px] tracking-wide relative z-50">
+      <div className="flex flex-wrap items-center gap-5 w-full">
         <a href="javascript:void(0)">
-          <img src={TeamLogo} alt="logo" className="w-20" />
+          <img src={TeamLogo} alt="logo" className="w-24 max-lg:mr-5" />
         </a>
 
         <div
           id="collapseMenu"
-          className={`max-lg:${
+          className={`lg:!flex lg:ml-auto max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50 ${
             menuOpen ? "block" : "hidden"
-          } lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
+          }`}
         >
           <button
             id="toggleClose"
@@ -41,40 +41,40 @@ const Header = () => {
             </svg>
           </button>
 
-          <ul className="lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
+          <ul className="lg:flex gap-4 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
             <li className="mb-6 hidden max-lg:block">
               <a href="javascript:void(0)">
-                <img src={TeamLogo} alt="logo" className="w-36" />
+                <img src={TeamLogo} alt="logo" className="w-24" />
               </a>
             </li>
-            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                className="hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-[#007bff] font-semibold block text-[15px]"
               >
                 Home
               </a>
             </li>
-            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-[#333] font-semibold block text-[15px]"
               >
                 Schedule
               </a>
             </li>
-            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-[#333] font-semibold block text-[15px]"
               >
                 Roster
               </a>
             </li>
-            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-[#333] font-semibold block text-[15px]"
               >
                 Contact
               </a>
@@ -82,11 +82,11 @@ const Header = () => {
           </ul>
         </div>
 
-        <div className="flex max-lg:ml-auto space-x-3 lg:hidden">
+        <div className="flex items-center max-lg:ml-auto space-x-6">
           <button id="toggleOpen" onClick={handleClick} className="lg:hidden">
             <svg
               className="w-7 h-7"
-              fill="#000"
+              fill="#333"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,28 +1,37 @@
+import CoachesImg from "../assets/IMG_1999.jpg";
+
 const TeamSection = () => {
   return (
     <section className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">16U BANISTER</h1>
+      <h1 className="text-4xl font-bold mb-4">ROCK GOLD WALKER</h1>
       <img
-        src="https://athletics-mercado.com/wp-content/uploads/2022/09/image0.jpeg"
+        src={CoachesImg}
         alt="16U Banister Team"
-        className="w-full h-auto mb-8"
+        className="w-2/3 h-auto mb-8 mx-auto"
       />
       <div>
-        <h2 className="text-2xl font-semibold">16U BANISTER MANAGERS</h2>
+        <h2 className="text-2xl font-semibold">ROCK GOLD WALKER 14U COACHES</h2>
         <hr className="border-t-2 border-gold my-2" />
-        <p>Athletics Mercado Director: Sam Banister</p>
-        <p>Athletics Mercado Manager: Cheri Thiele</p>
-        <p>Head Coach: Sam Banister</p>
-        <p>Assistant Coach: Jameel Abdullah</p>
-        <p>athleticsmercadoflorida@gmail.com</p>
+        <p>Head Coach: Chan Walker</p>
+        <p>Assistant Coach: Ashlyn Katz</p>
+        <p>
+          <a
+            href="mailto:rockgoldwalker@gmail.com"
+            className="text-gold hover:text-yellow-600 underline"
+          >
+            rockgoldwalker@gmail.com
+          </a>
+        </p>
       </div>
       <div className="mt-8">
-        <h2 className="text-2xl font-semibold">16U BANISTER ROSTER</h2>
+        <h2 className="text-2xl font-semibold">ROCK GOLD 14U WALKER ROSTER</h2>
         <hr className="border-t-2 border-gold my-2" />
         {/* Add roster details here */}
       </div>
       <div className="mt-8">
-        <h2 className="text-2xl font-semibold">16U BANISTER TEAM LINKS</h2>
+        <h2 className="text-2xl font-semibold">
+          ROCK GOLD 14U WALKER TEAM LINKS
+        </h2>
         <hr className="border-t-2 border-gold my-2" />
         {/* Add team links here */}
       </div>
