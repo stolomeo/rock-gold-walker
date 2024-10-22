dcf6d8e2e9b24cff349ce881cb32e737dae76d24
stolomeo <tolomeosebastian@gmail.com>
2024-10-22 16:28:41 -0400

    add team image

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,23 +1,23 @@
-import CoachesImg from "../assets/IMG_1999.jpg";
+import TeamImg from "../assets/team_roster.jpg";
 
 const TeamSection = () => {
   return (
-    <section className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">ROCK GOLD WALKER</h1>
+    <section className="container p-4 mx-auto">
+      <h1 className="mb-4 text-4xl font-bold">ROCK GOLD WALKER</h1>
       <img
-        src={CoachesImg}
+        src={TeamImg}
         alt="16U Banister Team"
-        className="w-2/3 h-auto mb-8 mx-auto"
+        className="h-auto mx-auto mb-8"
       />
       <div>
         <h2 className="text-2xl font-semibold">ROCK GOLD WALKER 14U COACHES</h2>
-        <hr className="border-t-2 border-gold my-2" />
+        <hr className="my-2 border-t-2 border-gold" />
         <p>Head Coach: Chan Walker</p>
         <p>Assistant Coach: Ashlyn Katz</p>
         <p>
           <a
             href="mailto:rockgoldwalker@gmail.com"
-            className="text-gold hover:text-yellow-600 underline"
+            className="underline text-gold hover:text-yellow-600"
           >
             rockgoldwalker@gmail.com
           </a>
@@ -25,19 +25,19 @@ const TeamSection = () => {
       </div>
       <div className="mt-8">
         <h2 className="text-2xl font-semibold">ROCK GOLD 14U WALKER ROSTER</h2>
-        <hr className="border-t-2 border-gold my-2" />
+        <hr className="my-2 border-t-2 border-gold" />
         {/* Add roster details here */}
       </div>
       <div className="mt-8">
         <h2 className="text-2xl font-semibold">
           ROCK GOLD 14U WALKER TEAM LINKS
         </h2>
-        <hr className="border-t-2 border-gold my-2" />
+        <hr className="my-2 border-t-2 border-gold" />
         {/* Add team links here */}
       </div>
       <div className="mt-8">
         <h2 className="text-2xl font-semibold">TEAM NEWS</h2>
-        <hr className="border-t-2 border-gold my-2" />
+        <hr className="my-2 border-t-2 border-gold" />
         {/* Add team news here */}
       </div>
     </section>
