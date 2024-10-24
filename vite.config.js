e363e384182774224cc780123ee763be33f0adb3
stolomeo <tolomeosebastian@gmail.com>
2024-10-23 22:07:11 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { ghPages } from "vite-plugin-gh-pages";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/rock-gold-walker",
+  base: "/",
 });
